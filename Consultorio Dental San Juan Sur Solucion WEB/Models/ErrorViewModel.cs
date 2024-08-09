f2e596de3cb9abb9d11b544007db29f1ef48d86e
christ48 <christopheralvarez220302@gmail.com>
2024-08-08 19:55:18 -0600

    Agregar archivos de proyecto.

---
+++ Consultorio Dental San Juan Sur Solucion WEB/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Consultorio_Dental_San_Juan_Sur_Solucion_WEB.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
